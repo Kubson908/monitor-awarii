ac3bf7cb0d8a3e5128dd4bc1888e20c8cc3f5c38
jakub-cz01 <jakub.czajkowski01@gmail.com>
2023-03-14 12:18:22 +0100

    Add .env

--- api/src/core/database/database.provider.ts
+++ api/src/core/database/database.provider.ts
@@ -1,19 +1,6 @@
-import { Awaria, Pracownik, Raport, Stanowisko } from './entities'
+import { DB_Config } from './config/database.config.js';
+import { DataSource } from 'typeorm';
 
-import { DataSource } from "typeorm"
+const dataSource = new DataSource(DB_Config);
 
-
-
-const dataSource = new DataSource({
-    type: "mssql",
-    host: "uwm-team-project.database.windows.net",
-    port: 1433,
-    database: "Team_Project_DB",
-    username: "root1",
-    password: "JSp70mnwksno",
-    entities: [Awaria, Pracownik, Raport, Stanowisko],
-    synchronize: true,
-
-})
-  
-export { dataSource }
+export { dataSource };
