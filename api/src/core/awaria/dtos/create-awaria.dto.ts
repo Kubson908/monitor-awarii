a26ab63b1440fd18fae5f6559b3febb9ac08c638
Kuba <kubson908@wp.pl>
2023-03-25 21:15:00 +0100

    fixed status while creating awaria
    Co-authored-by: adriandlugolecki <adriandlugolecki@users.noreply.github.com>
    Co-authored-by: jakub-cz01 <jakub-cz01@users.noreply.github.com>

--- api/src/core/awaria/dtos/create-awaria.dto.ts
+++ api/src/core/awaria/dtos/create-awaria.dto.ts
@@ -1,11 +1,9 @@
-import { PriorityType, StatusType } from '../../../typings/awaria.types'
+import { PriorityType } from '../../../typings/awaria.types'
 import { IsString, IsNumber} from 'class-validator'
 
 export class CreateAwariaDto {
   @IsString()
   opis_awarii: string;
-  @IsNumber()
-  status: StatusType;
   @IsNumber()
   priorytet: PriorityType;
   @IsNumber()
