961478d37910fa14b970af5edcbfa56dff832173
Kuba <kubson908@wp.pl>
2023-03-29 00:06:59 +0200

    last working commit

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.enableCors({
-    origin: "http://localhost:3000",
+    origin: 'http://localhost:3000',
     credentials: true,
   });
   await app.setGlobalPrefix('api');
