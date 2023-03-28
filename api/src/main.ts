fe6c43c4139db9cc9756dbfcde26d205a5686136
Kuba <kubson908@wp.pl>
2023-03-28 23:23:44 +0200

    test 6

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,8 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.enableCors();
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*',
+      credentials: true,
+    },
+  });
   await app.setGlobalPrefix('api');
   await app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen(process.env.PORT);
