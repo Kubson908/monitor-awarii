c4728286d580a146c4c9960d4b029b9f4fa3e623
Kuba <kubson908@wp.pl>
2023-03-28 23:38:17 +0200

    undo test

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,11 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: '*',
-      credentials: false,
-    },
+  const app = await NestFactory.create(AppModule);
+  await app.enableCors({
+    origin: process.env.ORIGIN,
+    credentials: true,
   });
   await app.setGlobalPrefix('api');
   await app.useGlobalPipes(new ValidationPipe({ transform: true }));
