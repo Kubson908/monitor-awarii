54febe4195445109936e7ffb786a001b5bcb264c
Kuba <kubson908@wp.pl>
2023-04-17 12:23:19 +0200

    added live stream server, pracownik list function
    Co-authored-by: jakub-cz01 <jakub-cz01@users.noreply.github.com>
    Co-authored-by: adriandlugolecki <adriandlugolecki@users.noreply.github.com>
    Co-authored-by: Carind <Carind@users.noreply.github.com>

--- api/src/core/pracownik/controllers/pracownik.controller.ts
+++ api/src/core/pracownik/controllers/pracownik.controller.ts
@@ -1,10 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { CreatePracownikDto } from '../dtos/create-pracownik.dto';
 import { PracownikService } from '../services/pracownik.service';
 
 @Controller('pracownicy')
 export class PracownikController {
     constructor(private pracownikService: PracownikService) {}
+    @Get('/lista')
+    pracownikList(){
+        return this.pracownikService.pracownikList();
+    }
+
     @Post('/dodaj')
     addPracownik(@Body() createPracownikDto: CreatePracownikDto) {
         return this.pracownikService.addPracownik(createPracownikDto);
